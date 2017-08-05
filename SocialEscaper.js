98d2f72415a14e67a70424015b365210575f118d
nat-lee <nat-lee@192.168.0.108>
2017-08-05 21:55:58 +0800

    All code for fb and line

---
+++ SocialEscaper.js
@@ -0,0 +1,52 @@
+$(function() {
+    // Test to be replaced
+    var logo_file = "logo.jpg";
+    var socesc_txt_head = "You are browsing within __APP__ __PF__ application";
+    var socesc_txt_body1 = "This could possibly cause unexpected issues.";
+    var socesc_txt_body2 = "Please click the menu button (as show below) for navigating to your favorite browser.";
+    var instruction_file = "instruction_{{APP}}_file.jpg";
+    var secesc_txt_cont = "Continue using in __APP__";
+    // CODE
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    // TEST ************************************
+    userAgent = "Fake user agent: FBAN iPhone";
+    // TEST ************************************
+    function chkMobilePlatform() {
+        var platform = "";
+        if (userAgent.indexOf("iPhone") > -1) {
+            platform = "iPhone";
+        } else if (userAgent.indexOf("Android") > -1) {
+            platform = "Android";
+        }
+        return platform;
+    }
+    function chkSocialApp() {
+        var app = "";
+        if ((userAgent.indexOf("FBAN") > -1) || (userAgent.indexOf("FBAV") > -1)) {
+            app = "Facebook";
+        } else if (userAgent.indexOf("Line") > -1) {
+            app = "Line";
+        }
+        return app;
+    }
+    var socialApp = chkSocialApp();
+    if ("" !== socialApp) {
+        var platform = chkMobilePlatform() || "mobile";
+        socesc_txt_head = socesc_txt_head.replace("__APP__", socialApp).replace("__PF__", platform);
+        secesc_txt_cont = secesc_txt_cont.replace("__APP__", socialApp);
+        instruction_file = instruction_file.replace("{{APP}}", socialApp);
+        $("body").append('<div id="socesc_wrapper">'+
+                    '<div id="socesc_right_btn"><a id="socesc_close_btn">&times;</a></div>'+
+                    '<img src="'+logo_file+'" />'+
+                    '<h1>'+socesc_txt_head+'</h1>'+
+                    '<p>'+socesc_txt_body1+'</p>'+
+                    '<p>'+socesc_txt_body2+'</p>'+
+                    '<img src="'+instruction_file+'" />'+
+                    '<p><a id="socesc_close_txt">'+secesc_txt_cont+'</a></p>'+
+                '</div>');
+    }
+    $("#socesc_close_txt, #socesc_close_btn").click(function (e) {
+	e.preventDefault();
+	$("#socesc_wrapper").slideUp();
+    });
+});
---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <script
+            src="http://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
+        <script src="SocialEscaper.js"></script>
+        <link href="SocialEscaper.css" rel="StyleSheet" />
+        <title>Social Escaper</title>
+    </head>
+    <body>
+        <h1>Social Escaper</h1>
+        <p>If this page is opened in social network's embeded browser, the popup window would appear and instruct the user to escape from it.</p>
+        <p>Required library: jQuery</p>
+    </body>
+</html>
